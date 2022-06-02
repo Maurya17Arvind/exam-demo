7e68fdee0026479d22399d7640c85713ca510121
arvind-tagline <arvind.tagline@gmail.com>
2022-06-02 11:47:20 +0530

    add SwUpdate

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { NavigationCancel, NavigationEnd, NavigationError, NavigationStart, Router, RouterEvent } from '@angular/router';
+import { SwUpdate, UnrecoverableStateEvent, VersionEvent, VersionReadyEvent } from '@angular/service-worker';
 import { NgxSpinnerService } from 'ngx-spinner';
 
 @Component({
@@ -7,15 +8,41 @@ import { NgxSpinnerService } from 'ngx-spinner';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'exam-demo';
 
-  constructor(private router: Router, private spinner: NgxSpinnerService) {
+  constructor(private router: Router, private spinner: NgxSpinnerService, private serviceWorker: SwUpdate) {
     this.router.events.subscribe((e: RouterEvent) => {
       this.navigationInterceptor(e);
     })
   }
-  navigationInterceptor(event: RouterEvent): void {
+
+  ngOnInit(): void {
+    if (this.serviceWorker.isEnabled) {
+      return;
+    }
+    this.handelVersionUpdate();
+  }
+
+  public handelVersionUpdate(): void {
+    this.serviceWorker.versionUpdates.subscribe((event: VersionEvent) => {
+      alert(event.type);
+      if (event.type === "VERSION_READY" &&
+        confirm(
+          `New version ${(event as VersionReadyEvent).latestVersion.hash} available. Load New Version ?`
+        )
+      ) {
+        window.location.reload();
+      }
+    });
+
+    this.serviceWorker.unrecoverable.subscribe((event: UnrecoverableStateEvent) => {
+      alert('Error reason :' + event.reason);
+    })
+  }
+
+
+  public navigationInterceptor(event: RouterEvent): void {
     if (event instanceof NavigationStart) {
       this.spinner.show();
     }
