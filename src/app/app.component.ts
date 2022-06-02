ecf9a4f4615fd0fcf77af471ba1953d76cd72427
arvind-tagline <arvind.tagline@gmail.com>
2022-06-02 16:38:13 +0530

    remove unused code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { NavigationCancel, NavigationEnd, NavigationError, NavigationStart, Router, RouterEvent } from '@angular/router';
-import { SwUpdate, UnrecoverableStateEvent, VersionEvent, VersionReadyEvent } from '@angular/service-worker';
+import { SwPush, SwUpdate, UnrecoverableStateEvent, VersionEvent, VersionReadyEvent } from '@angular/service-worker';
 import { NgxSpinnerService } from 'ngx-spinner';
 
 @Component({
@@ -10,8 +10,15 @@ import { NgxSpinnerService } from 'ngx-spinner';
 })
 export class AppComponent implements OnInit {
   title = 'exam-demo';
+  readonly VAPID_PUBLIC_KEY = "BOBlBr9Yz5VqIb-MR49NHz4f-uIEklTjskFK28WlTYD3iZ3myNKqDLHTx-lFyZ_J9Q5_8mKd0rn-dzGlrQ0X8nY";
 
-  constructor(private router: Router, private spinner: NgxSpinnerService, private serviceWorker: SwUpdate) {
+
+  constructor(private router: Router,
+    private spinner: NgxSpinnerService,
+    private serviceWorker: SwUpdate,
+    private pushService: SwPush,
+    // private notificationService: NewsletterService
+  ) {
     this.router.events.subscribe((e: RouterEvent) => {
       this.navigationInterceptor(e);
     })
@@ -24,12 +31,13 @@ export class AppComponent implements OnInit {
       return;
     }
     this.handelVersionUpdate();
+    this.pushNotifications();
   }
 
+  //On reload site/app show alert if version update
   public handelVersionUpdate(): void {
     this.serviceWorker.versionUpdates.subscribe((event: VersionEvent) => {
       console.log('event', event)
-      alert(event.type);
       if (event.type === "VERSION_READY" &&
         confirm(
           `New version ${(event as VersionReadyEvent).latestVersion.hash} available. Load New Version ?`
@@ -42,6 +50,34 @@ export class AppComponent implements OnInit {
     this.serviceWorker.unrecoverable.subscribe((event: UnrecoverableStateEvent) => {
       alert('Error reason :' + event.reason);
     })
+  }
+  subscribeToNotifications() {
+    // this.pushService.requestSubscription({
+    //   serverPublicKey: this.VAPID_PUBLIC_KEY
+    // })
+    //   .then(sub => this.newsletterService.addPushSubscriber(sub).subscribe())
+    //   .catch(err => console.error("Could not subscribe to notifications", err));
+  }
+
+  public async pushNotifications() {
+    // console.log('pushNotifications')
+    // try {
+    //   const sub = await this.pushService.requestSubscription({
+    //     serverPublicKey: "BOBlBr9Yz5VqIb-MR49NHz4f-uIEklTjskFK28WlTYD3iZ3myNKqDLHTx-lFyZ_J9Q5_8mKd0rn-dzGlrQ0X8nY"
+    //   });
+    //   // this.notificationService.addSubscription(sub);
+    // } catch (error) {
+    //   console.error('Could not subscribe due to :', error);
+    // }
+    // this.pushService.messages.subscribe((message) => {
+    //   console.log(message);
+    // });
+    // this.pushService.notificationClicks.subscribe((message) => {
+    //   console.log(message);
+    // });
+    // this.pushService.subscription.subscribe((subscription) => {
+    //   console.log(subscription);
+    // });
   }
 
 
